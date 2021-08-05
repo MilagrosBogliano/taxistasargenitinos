0ffe512b193991463c264f53dda9e14e844976ab
Milagros Bogliano <milagrosbogliano@hotmail.com>
2021-08-05 16:13:29 -0300

    Poniendo en funcionamiento el formulario

--- enviar.php
+++ enviar.php
@@ -1,25 +1,22 @@
 <?php
-$motivo = $_POST['motivo']
+//Llamando a los campos
+$motivo = $_POST['motivo'];
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
-$mail = $_POST['email'];
+$email = $_POST['email'];
 $telefono = $_POST['telefono'];
-$empresa = $_POST['comentario'];
+$comentario = $_POST['comentario'];
+//Datos para el correo
+$destinatario = "taxistasargentinos@gmail.com";
+$asunto = "Contacto desde la pagina web";
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$carta = "De: $nombre \n";
+$carta .= "Email: $email \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Comentario: $comentario";
 
-$comentario = "Este comentario fue enviado por " . $nombre $apellido . ",\r\n";
-$comentario .= "Su e-mail es: " . $mail . " \r\n";
-$comentario .= "Comentario: " . $_POST['comentario'] . " \r\n";
-$comentario .= "Enviado el " . date('d/m/Y', time());
+//Enviando mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje_envio.html')
 
-$para = 'taxistasargentinos@gmail.com';
-$asunto = 'Mensaje del sitio web';
-
-mail($para, $asunto, utf8_decode($comentario), $header);
-
-header("Location:index.html");
 ?>
